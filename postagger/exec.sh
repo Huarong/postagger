fe8fca3e657324d09677d19ffdf10f3223e70531
Huo Huarong <huohuarong@gmail.com>
2014-06-12 11:18:23 +0800

    modify exec.sh

--- postagger/exec.sh
+++ postagger/exec.sh
@@ -1,3 +1,3 @@
 
-crf_learn -f 3 template ../data/crf_train.tag model
-# crf_test -m model ../data/crf_train.tag.backup
+crf_learn -f 4 -p 2 template ../data/9_10_train.tag model9_10
+#crf_test -m model9_10 ../data/1_10_train.tag > 1_10.txt
