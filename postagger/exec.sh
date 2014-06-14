cd6594d062f0571ca47e897087e7b30d821c5b1f
Huo Huarong <huohuarong@gmail.com>
2014-06-14 10:27:05 +0800

    ignore result directory

--- postagger/exec.sh
+++ postagger/exec.sh
@@ -1,3 +1,3 @@
 
-crf_learn -f 4 -p 2 template ../data/9_10_train.tag model9_10
-#crf_test -m model9_10 ../data/1_10_train.tag > 1_10.txt
+crf_learn -f 4 -p 2 -c 2 template ../data/9_10_train.tag ../result/model9_10_f4c2
+crf_test -m ../result/model9_10_f4c2 ../data/1_10_train.tag > ../result/1_10_f4c2.txt
