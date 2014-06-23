2a3690c7a34b0dc568a10778627715b103d87d5d
Huo Huarong <huohuarong@gmail.com>
2014-06-23 10:20:00 +0800

    split the training data with 7-3

--- postagger/split_train_data.py
+++ postagger/split_train_data.py
@@ -5,7 +5,7 @@
 import util
 
 
-def split(path, small_path, large_path, parts=10):
+def split(path, small_path, large_path, parts=10, small=3):
     with open(path) as f:
         for index, line in enumerate(f):
             pass
@@ -13,8 +13,9 @@ def split(path, small_path, large_path, parts=10):
         print 'Total line number is: %d' % total_line_num
         a_part_num = total_line_num / parts
         f.seek(0)
-        for i in range(a_part_num - 1):
-            line = f.readline()
+        for i in range(small):
+            for j in range(a_part_num - 1):
+                line = f.readline()
         # find a blank line
         line_offset = a_part_num - 1
         offset = f.tell()
@@ -34,10 +35,11 @@ def split(path, small_path, large_path, parts=10):
 
 def main():
     parts = 10
+    small = 3
     train_path = util.abs_path('data/crf_train.tag')
-    out_path_train = util.abs_path('data/%d_%d_train.tag' % (parts - 1, parts))
-    out_path_validation = util.abs_path('data/1_%d_train.tag' % parts)
-    split(train_path, out_path_validation, out_path_train, parts=parts)
+    out_path_train = util.abs_path('data/%d_%d_train.tag' % (parts - small, parts))
+    out_path_validation = util.abs_path('data/%d_%d_train.tag' % (small, parts))
+    split(train_path, out_path_validation, out_path_train, parts=parts, small=small)
 
 
 if __name__ == '__main__':
